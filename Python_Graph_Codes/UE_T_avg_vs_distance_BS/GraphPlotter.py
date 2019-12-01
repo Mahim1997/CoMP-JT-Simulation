f3295dad59c143ff4084a28c2970832a6158f0d3
Mahim1997 <38731142+Mahim1997@users.noreply.github.com>
2019-12-01 06:20:11 +0600

    Read data porjonto hoise
    Graph subplots kora baaki ache

--- Python_Graph_Codes/UE_T_avg_vs_distance_BS/GraphPlotter.py
+++ Python_Graph_Codes/UE_T_avg_vs_distance_BS/GraphPlotter.py
@@ -7,23 +7,58 @@ print("Hello there")
 
 
 class Datas_each_JT():
+    def __init__(self):
+        self.distance_col = []
+        self.chi_0_1_col = []
+        self.chi_0_2_col = []
+        self.chi_0_3_col = []
+        self.chi_0_4_col = []
+        self.chi_0_5_col = []
+        self.chi_0_6_col = []
+        self.chi_0_7_col = []
+        self.chi_0_8_col = []
+        self.chi_0_9_col = []
+        self.chi_1_col = []
     
+    def read_from_csv(self, fileName):
+        with open(fileName, 'r') as csvFile:
+            readCSV = csv.reader(csvFile, delimiter=',')
+            num_rows = -1
+            for row in readCSV:
+                num_rows = num_rows + 1
+                if num_rows != 0:
+                    self.distance_col.append(row[0])
+                    self.chi_0_1_col.append(row[1])
+                    self.chi_0_2_col.append(row[2])
+                    self.chi_0_3_col.append(row[3])
+                    self.chi_0_4_col.append(row[4])
+                    self.chi_0_5_col.append(row[5])
+                    self.chi_0_6_col.append(row[6])
+                    self.chi_0_7_col.append(row[7])
+                    self.chi_0_8_col.append(row[8])
+                    self.chi_0_9_col.append(row[9])
+                    self.chi_1_col.append(row[10])
+    
+    def print_first_elements(self):
+        print("self.distance_col[0] = ", self.distance_col[0])
+        print("self.chi_0_1_col[0] = ", self.chi_0_1_col[0])
+        print("self.chi_0_2_col[0] = ", self.chi_0_2_col[0])
+        print("self.chi_0_3_col.size = " + str(len(self.chi_0_3_col)))
 
-def read_from_csv(fileName):
-    distance_col = []
-    chi_0_1_col = []
-    chi_0_2_col = []
-    chi_0_3_col = []
-    chi_0_4_col = []
-    chi_0_5_col = []
-    chi_0_6_col = []
-    chi_0_7_col = []
-    chi_0_8_col = []
-    chi_0_9_col = []
-    chi_1_col = []
+ob_jt_0 = Datas_each_JT()
+ob_jt_0.read_from_csv("UE_T_avg_vs_distance_BS_MC_1000_JT_0_Take_after_calcs.csv")
+# ob_jt_0.print_first_elements()
 
+ob_jt_1 = Datas_each_JT()
+ob_jt_1.read_from_csv("UE_T_avg_vs_distance_BS_MC_1000_JT_1_Take_after_calcs.csv")
+# ob_jt_1.print_first_elements()
 
-read_from_csv("UE_T_avg_vs_distance_BS_MC_1000_JT_1_Take_after_calcs.csv")
+ob_jt_2 = Datas_each_JT()
+ob_jt_2.read_from_csv("UE_T_avg_vs_distance_BS_MC_1000_JT_2_Take_after_calcs.csv")
+
+ob_jt_3 = Datas_each_JT()
+ob_jt_3.read_from_csv("UE_T_avg_vs_distance_BS_MC_1000_JT_3_Take_after_calcs.csv")
+
 
 
 ##x = [1,2,3]
